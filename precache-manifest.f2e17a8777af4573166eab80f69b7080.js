4103eada14dcc38b1fc71f3c4b038274a67c823a
nestor.carrasquero <nestor.carrasquero@gmail.com>
2020-06-07 21:26:02 -0400

    Updates

---
+++ precache-manifest.f2e17a8777af4573166eab80f69b7080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fde40144d9df513dfca41d346590ad",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "a0992c839440c7c0bcdb",
+    "url": "/hello-world/static/css/2.4aa2b00b.chunk.css"
+  },
+  {
+    "revision": "809d949ef9a84e87c730",
+    "url": "/hello-world/static/css/main.46d4cbc4.chunk.css"
+  },
+  {
+    "revision": "a0992c839440c7c0bcdb",
+    "url": "/hello-world/static/js/2.8be87f49.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/hello-world/static/js/2.8be87f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809d949ef9a84e87c730",
+    "url": "/hello-world/static/js/main.75ac5930.chunk.js"
+  },
+  {
+    "revision": "a600d5ef8742d454af0c",
+    "url": "/hello-world/static/js/runtime-main.551fc92d.js"
+  }
+]);
