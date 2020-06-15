56ec3a1834d82ad4ff902a48324c357a1ce1ce82
nestor.carrasquero <nestor.carrasquero@gmail.com>
2020-06-15 15:37:02 -0400

    Updates

--- precache-manifest.7e81c2d8a172c19000ca028a5767e682.js
+++ precache-manifest.7e81c2d8a172c19000ca028a5767e682.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a1666b90e103178b8b620033baddf7d0",
+    "revision": "7f19a73296c5e3dd8b19153750384ad8",
     "url": "/hello-world/index.html"
   },
   {
-    "revision": "a0992c839440c7c0bcdb",
+    "revision": "da39532504270afe7dec",
     "url": "/hello-world/static/css/2.4aa2b00b.chunk.css"
   },
   {
-    "revision": "882b5c87bacda9ac2118",
+    "revision": "5b749e346f88f7e3df85",
     "url": "/hello-world/static/css/main.46d4cbc4.chunk.css"
   },
   {
-    "revision": "a0992c839440c7c0bcdb",
-    "url": "/hello-world/static/js/2.8be87f49.chunk.js"
+    "revision": "da39532504270afe7dec",
+    "url": "/hello-world/static/js/2.d7f18047.chunk.js"
   },
   {
     "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
-    "url": "/hello-world/static/js/2.8be87f49.chunk.js.LICENSE.txt"
+    "url": "/hello-world/static/js/2.d7f18047.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "882b5c87bacda9ac2118",
-    "url": "/hello-world/static/js/main.ae959d8b.chunk.js"
+    "revision": "5b749e346f88f7e3df85",
+    "url": "/hello-world/static/js/main.5bbd3990.chunk.js"
   },
   {
     "revision": "a600d5ef8742d454af0c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hello-world/precache-manifest.e90686818349b17573f61d50e943caec.js"
+  "/hello-world/precache-manifest.7e81c2d8a172c19000ca028a5767e682.js"
 );
 
 self.addEventListener('message', (event) => {
